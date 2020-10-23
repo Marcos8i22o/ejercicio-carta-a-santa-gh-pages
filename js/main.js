374475ef3ad5f5242124e6f13690a4804f036fc3
Marcos8i22o <marcos.jinno@gmail.com>
2020-10-23 12:56:50 -0300

    agrega validaciones de los campos nombre y ciudad

--- js/main.js
+++ js/main.js
@@ -5,5 +5,24 @@ const ciudad = $form.ciudad.value;
 const comportamiento = $form.comportamiento.value;
 const descripcionRegalo = $form['descripcion-regalo'].value;
 
+function validarNombre (nombre) {
+    if (nombre.length === 0) {
+        return 'Este campo debe tener al menos 1 caracter';
+    }
+    if (nombre.length >= 50) {
+        return 'Este campo debe tener menos de 50 caracteres';
+    }
+
+    return '';
+}
+
+function validarCiudad (ciudad) {
+    if (ciudad.length === 0) {
+        return 'El campo ciudad debe contener al menos 1 caracter';
+    }
+    return '';
+}
 
 //console.log(descripcionRegalo)
+validarNombre();
+validarCiudad();
