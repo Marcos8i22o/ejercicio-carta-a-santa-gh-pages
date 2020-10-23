d2229caa7e48cfc48359e7e94fc318f38273d92a
Marcos8i22o <marcos.jinno@gmail.com>
2020-10-23 12:33:05 -0300

    obtener valores del formulario

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const $form = document.querySelector('#carta-a-santa');
+
+const nombre = $form.nombre.value;
+const ciudad = $form.ciudad.value;
+const comportamiento = $form.comportamiento.value;
+const descripcionRegalo = $form['descripcion-regalo'].value;
+
+
+//console.log(descripcionRegalo)
