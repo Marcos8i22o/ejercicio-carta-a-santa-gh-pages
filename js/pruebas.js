44e9b7c3d564302e7d320a76d1b748acabf27116
Marcos8i22o <marcos.jinno@gmail.com>
2020-10-23 12:57:19 -0300

    agrega pruebas de validación de las validaciones de los campos nombre y ciudad

--- js/pruebas.js
+++ js/pruebas.js
@@ -1,15 +1,25 @@
 function probarValidarNombre() {
   console.assert(
-      validarNombre('') === 'Este campo debe tener al menos 1 caracter',
-      'Validar nombre no validó que el nombre no sea vacío',
+    validarNombre("") === "Este campo debe tener al menos 1 caracter",
+    "Validar nombre no validó que el nombre no sea vacío"
   );
 
   console.assert(
-      validarNombre(
-          '111111111111111111111111111111111111111111111111111111111111111111111111111111111111111') ===
-      'Este campo debe tener menos de 50 caracteres',
-      'Validar nombre no validó que el nombre sea menor a 50 caracteres',
+    validarNombre(
+      "111111111111111111111111111111111111111111111111111111111111111111111111111111111111111"
+    ) === "Este campo debe tener menos de 50 caracteres",
+    "Validar nombre no validó que el nombre sea menor a 50 caracteres"
+  );
+}
+
+function probarValidarCiudad() {
+  console.assert(
+    validarCiudad("") === "El campo ciudad debe contener al menos 1 caracter",
+    "Validar ciudad no validó que el campo ciudad no sea vacío"
   );
 }
 
 probarValidarNombre();
+probarValidarCiudad();
+
+
