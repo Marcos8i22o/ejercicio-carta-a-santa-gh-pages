c53b843b4efc2cea47523ff430bf2279cd2efc28
Marcos8i22o <marcos.jinno@gmail.com>
2020-10-30 10:23:44 -0300

    agrega borde rojo en el campo de error y lo informa

--- js/main.js
+++ js/main.js
@@ -1,65 +1,88 @@
-const $form = document.querySelector('#carta-a-santa');
+function validarNombre(nombre) {
+  const contieneSoloLetras = /^[A-z]+$/.test(nombre);
 
-const nombre = $form.nombre.value;
-const ciudad = $form.ciudad.value;
-const comportamiento = $form.comportamiento.value;
-const descripcionRegalo = $form['descripcion-regalo'].value;
+  if (nombre.length === 0) {
+    return "Este campo debe tener al menos 1 caracter";
+  }
+  if (nombre.length >= 50) {
+    return "Este campo debe tener menos de 50 caracteres";
+  }
 
-function validarNombre (nombre) {
-    const contieneSoloLetras = /^[A-z]+$/.test(nombre);
+  if (!contieneSoloLetras) {
+    return "Este campo sólo debe contener letras";
+  }
 
-    if (nombre.length === 0) {
-        return 'Este campo debe tener al menos 1 caracter';
-    }
-    if (nombre.length >= 50) {
-        return 'Este campo debe tener menos de 50 caracteres';
-    }
-
-    if (!contieneSoloLetras) {
-        return 'Este campo sólo debe contener letras';
-    }
-
-    return '';
+  return "";
 }
 
-function validarCiudad (ciudad) {
-    if (ciudad.length === 0) {
-        return 'El campo ciudad debe contener al menos 1 caracter';
-    }
-    return '';
+function validarCiudad(ciudad) {
+  if (ciudad.length === 0) {
+    return "El campo ciudad debe contener al menos 1 caracter";
+  }
+  return "";
 }
 
 function validarDescripcionRegalo(descripcionRegalo) {
-    const contieneSoloLetrasYNumeros = /^[a-z0-9]+$/i.test(descripcionRegalo);
+  const contieneSoloLetrasYNumeros = /^[a-z0-9]+$/i.test(descripcionRegalo);
 
-    if (descripcionRegalo.length === 0) {
-        return 'El campo descripcionRegalo debe contener al menos 1 caracter';
-    }
-    if (descripcionRegalo.length >= 100) {
-        return 'El campo descripcionRegalo debe contener menos de 100 caracteres';
-    }
-    if (!contieneSoloLetrasYNumeros){
-        return 'El campo descripcionRegalo sólo debe contener números y letras';
-    }
+  if (descripcionRegalo.length === 0) {
+    return "El campo descripcionRegalo debe contener al menos 1 caracter";
+  }
+  if (descripcionRegalo.length >= 100) {
+    return "El campo descripcionRegalo debe contener menos de 100 caracteres";
+  }
+  if (!contieneSoloLetrasYNumeros) {
+    return "El campo descripcionRegalo sólo debe contener números y letras";
+  }
 
-    return '';
+  return "";
 }
 
-// function validarForm() {
-//     const errorNombre = validarNombre(nombre);
-//     console.log(manejarErrores(errores));
-    
-// }
+function validarFormulario(event) {
+  const $form = document.querySelector("#carta-a-santa");
 
-// function manejarErrores(errores) {
-//     if (!errores) {
-//         console.log("éxito");
-//     } else {
-//         console.log(errores);
-//     }
-// }
+  const nombre = $form.nombre.value;
+  const ciudad = $form.ciudad.value;
+  const descripcionRegalo = $form["descripcion-regalo"].value;
 
+  const errores = {
+    nombre: validarNombre(nombre),
+    ciudad: validarCiudad(ciudad),
+    "descripcion-regalo": validarDescripcionRegalo(descripcionRegalo),
+  };
+
+  event.preventDefault();
+
+  manejarErrores(errores);
+}
+
+function manejarErrores(errores) {
+  let cuentaErrores = 0;
+  const $errores = document.querySelector("#errores");
+
+  const keys = Object.keys(errores);
+
+  keys.forEach(function (key) {
+    const error = errores[key];
+    if (error) {
+      $form[key].className = "error";
+
+      const $error = document.createElement("li");
+      $error.innerText = error;
+
+      $errores.appendChild($error);
+
+      cuentaErrores++;
+    } else {
+      $form[key].className = "";
+    }
+  });
+
+  return cuentaErrores;
+}
+
+const $form = document.querySelector("#carta-a-santa");
+$form.onsubmit = validarFormulario;
 
 validarNombre(nombre);
-validarCiudad(ciudad);
-
+//validarCiudad(ciudad);
